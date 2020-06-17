f9e2bf8b42c68154ea1f083b2f6fee0666bc9bb8
shootaa <60875264+shootaa@users.noreply.github.com>
2020-06-17 15:56:13 +0900

    リツイート機能完成。 いいね機能のHTML部分とテーブル結合完了

--- html/php-challenge/index.php
+++ html/php-challenge/index.php
@@ -11,20 +11,22 @@ if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
 	$member = $members->fetch();
 } else {
 	// ログインしていない
-	header('Location: login.php'); exit();
+	header('Location: login.php');
+	exit();
 }
 
 // 投稿を記録する
 if (!empty($_POST)) {
 	if ($_POST['message'] != '') {
-		$message = $db->prepare('INSERT INTO posts SET member_id=?, message=?, reply_post_id=?, created=NOW()');
+		$message = $db->prepare('INSERT INTO posts SET member_id=?, message=?,reply_post_id=?,created=NOW()');
 		$message->execute(array(
 			$member['id'],
 			$_POST['message'],
 			$_POST['reply_post_id']
 		));
 
-		header('Location: index.php'); exit();
+		header('Location: index.php');
+		exit();
 	}
 }
 
@@ -44,10 +46,10 @@ $page = min($page, $maxPage);
 $start = ($page - 1) * 5;
 $start = max(0, $start);
 
-$posts = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id ORDER BY p.created DESC LIMIT ?, 5');
+$posts = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, (SELECT posts.*, likescount FROM posts LEFT JOIN (SELECT post_id, COUNT(post_id) AS likescount FROM likes GROUP BY post_id) AS likestable ON posts.id=likestable.post_id)
+p WHERE m.id=p.member_id ORDER BY p.created DESC LIMIT ?, 5');
 $posts->bindParam(1, $start, PDO::PARAM_INT);
 $posts->execute();
-
 // 返信の場合
 if (isset($_REQUEST['res'])) {
 	$response = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id AND p.id=? ORDER BY p.created DESC');
@@ -56,19 +58,73 @@ if (isset($_REQUEST['res'])) {
 	$table = $response->fetch();
 	$message = '@' . $table['name'] . ' ' . $table['message'];
 }
+// RT機能
+if (isset($_REQUEST['rt'])) {
+	$retweet = $db->prepare('SELECT m.name, m.picture, p.* FROM members m, posts p WHERE m.id=p.member_id AND p.id=? ORDER BY p.created DESC');
+	$retweet->execute(array($_REQUEST['rt']));
+	$retweetTable = $retweet->fetch();
+	$message = $db->prepare('INSERT INTO posts SET member_id=?, message=?,retweet_post_id=?, reply_post_id=?,created=NOW()');
+	$message->execute(array(
+		$member['id'],
+		$retweetTable['message'],
+		$retweetTable['id'],
+		$retweetTable['reply_post_id'],
+	));
+	//RTの件数表示
+	$message = $db->prepare('SELECT retweet_post_id,count(retweet_post_id) AS retweetcount FROM posts WHERE id=? GROUP BY retweet_post_id');
+	$message->execute(array(
+		$retweetTable['id'],
+	));
+	$retweetCount = $message->fetch();
+	$retweetInsert = $db->prepare('UPDATE posts SET retweetcount=? WHERE id=?');
+	$retweetInsert->execute(array(
+		$retweetCount['retweetcount'],
+		$retweetTable['id'],
+	));
+	$retweetInsert = $db->prepare('UPDATE posts SET retweetcount=? WHERE retweet_post_id=?');
+	$retweetInsert->execute(array(
+		$retweetCount['retweetcount'],
+		$retweetTable['id'],
+	));
+	//RTもう一回押すと削除
+	$retweetDel = $db->prepare('SELECT member_id,retweet_post_id FROM posts WHERE id=?');
+	$retweetDel->execute(array(
+		$_REQUEST['rt']
+	));
+	$retweetDelTable = $retweetDel->fetch();
+	if ($retweetDelTable['member_id'] === $member['id'] && $retweetDelTable['retweet_post_id'] > 0) {
+		$retweetDelDone = $db->prepare('DELETE FROM posts WHERE id=?');
+		$retweetDelDone->execute(array(
+			$_REQUEST['rt'],
+		));
+		$retweetDelDone = $db->prepare('DELETE FROM posts WHERE retweet_post_id=?');
+		$retweetDelDone->execute(array(
+			$_REQUEST['rt'],
+		));
+	};
+	header('Location: index.php');
+	exit();
+};
+
+//いいね機能
+
+
 
 // htmlspecialcharsのショートカット
-function h($value) {
+function h($value)
+{
 	return htmlspecialchars($value, ENT_QUOTES, 'UTF-8');
 }
 
 // 本文内のURLにリンクを設定します
-function makeLink($value) {
-	return mb_ereg_replace("(https?)(://[[:alnum:]\+\$\;\?\.%,!#~*/:@&=_-]+)", '<a href="\1\2">\1\2</a>' , $value);
+function makeLink($value)
+{
+	return mb_ereg_replace("(https?)(://[[:alnum:]\+\$\;\?\.%,!#~*/:@&=_-]+)", '<a href="\1\2">\1\2</a>', $value);
 }
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -79,82 +135,93 @@ function makeLink($value) {
 </head>
 
 <body>
-<div id="wrap">
-  <div id="head">
-    <h1>ひとこと掲示板</h1>
-  </div>
-  <div id="content">
-  	<div style="text-align: right"><a href="logout.php">ログアウト</a></div>
-    <form action="" method="post">
-      <dl>
-        <dt><?php echo h($member['name']); ?>さん、メッセージをどうぞ</dt>
-        <dd>
-          <textarea name="message" cols="50" rows="5"><?php echo h($message); ?></textarea>
-          <input type="hidden" name="reply_post_id" value="<?php echo h($_REQUEST['res']); ?>" />
-        </dd>
-      </dl>
-      <div>
-        <p>
-          <input type="submit" value="投稿する" />
-        </p>
-      </div>
-    </form>
+	<div id="wrap">
+		<div id="head">
+			<h1>ひとこと掲示板</h1>
+		</div>
+		<div id="content">
+			<div style="text-align: right"><a href="logout.php">ログアウト</a></div>
+			<form action="" method="post">
+				<dl>
+					<dt><?php echo h($member['name']); ?>さん、メッセージをどうぞ</dt>
+					<dd>
+						<textarea name="message" cols="50" rows="5"><?php echo h($message); ?></textarea>
+						<input type="hidden" name="reply_post_id" value="<?php echo h($_REQUEST['res']); ?>" />
+					</dd>
+				</dl>
+				<div>
+					<p>
+						<input type="submit" value="投稿する" />
+					</p>
+				</div>
+			</form>
 
-<?php
-foreach ($posts as $post):
-?>
-    <div class="msg">
-    <img src="member_picture/<?php echo h($post['picture']); ?>" width="48" height="48" alt="<?php echo h($post['name']); ?>" />
-    <p><?php echo makeLink(h($post['message'])); ?><span class="name">（<?php echo h($post['name']); ?>）</span>[<a href="index.php?res=<?php echo h($post['id']); ?>">Re</a>]</p>
-    <p class="day"><a href="view.php?id=<?php echo h($post['id']); ?>"><?php echo h($post['created']); ?></a>
-		<?php
-if ($post['reply_post_id'] > 0):
-?>
-<a href="view.php?id=<?php echo
-h($post['reply_post_id']); ?>">
-返信元のメッセージ</a>
-<?php
-endif;
-?>
-<?php
-if ($_SESSION['id'] == $post['member_id']):
-?>
-[<a href="delete.php?id=<?php echo h($post['id']); ?>"
-style="color: #F33;">削除</a>]
-<?php
-endif;
-?>
-    </p>
-    </div>
-<?php
-endforeach;
-?>
+			<?php
 
-<ul class="paging">
-<?php
-if ($page > 1) {
-?>
-<li><a href="index.php?page=<?php print($page - 1); ?>">前のページへ</a></li>
-<?php
-} else {
-?>
-<li>前のページへ</li>
-<?php
-}
-?>
-<?php
-if ($page < $maxPage) {
-?>
-<li><a href="index.php?page=<?php print($page + 1); ?>">次のページへ</a></li>
-<?php
-} else {
-?>
-<li>次のページへ</li>
-<?php
-}
-?>
-</ul>
-  </div>
-</div>
+			foreach ($posts as $post) :
+
+			?>
+				<div class="msg">
+					<?php if ($post['retweet_post_id'] > 0) : ?>
+						<p><?php echo h($post['name']) ?>さんがリツイートしました。</p>
+					<?Php endif; ?>
+					<img src="member_picture/<?php echo h($post['picture']); ?>" width="48" height="48" alt="<?php echo h($post['name']); ?>" />
+					<p><?php echo makeLink(h($post['message'])); ?><span class="name">（<?php echo h($post['name']); ?>）</span>[<a href="index.php?res=<?php echo h($post['id']); ?>">Re</a>]
+						<p class="day"><a href="view.php?id=<?php echo h($post['id']); ?>"><?php echo h($post['created']); ?></a></p>
+						<p class="rt"><a href="index.php?rt=<?php echo h($post['id']); ?>">RT<?php if ($post['retweet_post_id'] > 0) : ?><?php echo h($post['retweetcount']); ?>
+							<?Php endif; ?>
+							</a></p>
+							<p class="notlike"><a href="index.php?like=<?php $post['id']; ?>">♡:</a></p>
+							<p class="like"><a href="delete.php?like=<?php $post['id']; ?>">♡:</a></p>
+						<?php
+						if ($post['reply_post_id'] > 0) :
+						?>
+							<a href="view.php?id=<?php echo
+														h($post['reply_post_id']); ?>">
+								返信元のメッセージ</a>
+						<?php
+						endif;
+						?>
+
+						<?php
+						if ($_SESSION['id'] == $post['member_id']) :
+						?>
+							[<a href="delete.php?id=<?php echo h($post['id']); ?>" style="color: #F33;">削除</a>]
+						<?php
+						endif;
+						?>
+					</p>
+				</div>
+			<?php
+			endforeach;
+			?>
+
+			<ul class="paging">
+				<?php
+				if ($page > 1) {
+				?>
+					<li><a href="index.php?page=<?php print($page - 1); ?>">前のページへ</a></li>
+				<?php
+				} else {
+				?>
+					<li>前のページへ</li>
+				<?php
+				}
+				?>
+				<?php
+				if ($page < $maxPage) {
+				?>
+					<li><a href="index.php?page=<?php print($page + 1); ?>">次のページへ</a></li>
+				<?php
+				} else {
+				?>
+					<li>次のページへ</li>
+				<?php
+				}
+				?>
+			</ul>
+		</div>
+	</div>
 </body>
+
 </html>
